45b41db2a482df8ff33bc710b9bf942396693af8
Rishav Chourasia <rishav.chourasia@gmail.com>
2018-01-08 17:56:54 +0530

    Linear Algebra for Set Cover added for optimized update.

--- baselines/deepq/__init__.py
+++ baselines/deepq/__init__.py
@@ -1,6 +1,7 @@
 from baselines.deepq import models  # noqa
 from baselines.deepq.build_graph import build_act, build_train  # noqa
 from baselines.deepq.build_graph_PSO import build_act, build_train_PSO  # noqa
+from baselines.deepq.build_graph_swarm import build_act, build_train  # noqa
 
 from baselines.deepq.simple import learn, load  # noqa
 from baselines.deepq.replay_buffer import ReplayBuffer, PrioritizedReplayBuffer  # noqa
--- baselines/deepq/experiments/custom_cartpole_PSO.py
+++ baselines/deepq/experiments/custom_cartpole_PSO.py
@@ -10,7 +10,7 @@ import baselines.common.tf_util as U
 
 from baselines import logger
 from baselines import deepq
-from baselines.deepq.replay_buffer import ReplayBuffer
+from baselines.deepq.replay_buffer import BootstrappedReplayBuffer
 from baselines.common.schedules import LinearSchedule
 
 
@@ -23,8 +23,8 @@ def model(inpt, num_actions, scope, reuse=False):
         return out
 
 def arg_parse():
-    parser = argparse.ArgumentParser("Cartpole PSO_DQN test")
-    parser.add_argument("--num-clones", type=int, default=3, help="number of clones in PSO")
+    parser = argparse.ArgumentParser("Cartpole Swarm DQN test")
+    parser.add_argument("--num-clones", type=int, default=3, help="number of clones in swarm")
     return parser.parse_args()
 
 
@@ -34,18 +34,15 @@ if __name__ == '__main__':
         # Create the environment
         envs = [gym.make("CartPole-v0") for clone in range(args.num_clones)]
         # Create all the functions necessary to train the model
-        act_f_clones, train_clones, update_target_clones, debug_clones, pso_update = deepq.build_train_PSO(
+        act_f, train, update_target = deepq.build_train(
             make_obs_ph=lambda name: U.BatchInput(envs[0].observation_space.shape, name=name),
             q_func=model,
             num_actions=envs[0].action_space.n,
             optimizer=tf.train.AdamOptimizer(learning_rate=5e-4),
-            inertia=float(0.725),
-            social=float(0.2),
-            cognitive=float(0.2),
             num_clones=args.num_clones,
         )
         # Create the replay buffer
-        replay_buffers = [ReplayBuffer(50000) for clone in range(args.num_clones)]
+        replay_buffers = BootstrappedReplayBuffer(50000)
         # Create the schedule for exploration starting from 1 (every action is random) down to
         # 0.02 (98% of actions are selected according to values predicted by the model).
         exploration = LinearSchedule(schedule_timesteps=1000, initial_p=1, final_p=0.02)
@@ -55,8 +52,7 @@ if __name__ == '__main__':
 
         # Initialize the parameters and copy them to the target network.
         U.initialize()
-        for update_target in update_target_clones:
-            update_target()
+        update_target()
 
         episode_rewards = [[0.0] for clone in range(args.num_clones)]
         obs = []
@@ -66,10 +62,10 @@ if __name__ == '__main__':
         for t in itertools.count():
             for clone in range(args.num_clones):
                 # Take action and update exploration to the newest value
-                action = act_f_clones[clone](obs[clone][None], update_eps=exploration.value(t))[0]
-                new_obs, rew, done, _ = envs[clone].step(action)
+                action, cover_agents = act_f(obs[clone][None], clone, update_eps=exploration.value(t))
+                new_obs, rew, done, _ = envs[clone].step(action[0])
                 # Store transition in the replay buffer.
-                replay_buffers[clone].add(obs[clone], action, rew, new_obs, float(done))
+                replay_buffers.add(obs[clone], action[0], rew, new_obs, float(done), cover_agents[0])
                 obs[clone] = new_obs
 
                 episode_rewards[clone][-1] += rew
@@ -78,17 +74,17 @@ if __name__ == '__main__':
                     episode_rewards[clone].append(0)
 
                 is_solved = t > 100 and np.mean(episode_rewards[clone][-101:-1]) >= 200
+
                 if is_solved:
-                    # Show off the result
                     envs[clone].render()
                 else:
                     # Minimize the error in Bellman's equation on a batch sampled from replay buffer.
-                    if t > 200:
-                        obses_t, actions, rewards, obses_tp1, dones = replay_buffers[clone].sample(32)
-                        train_clones[clone](obses_t, actions, rewards, obses_tp1, dones, np.ones_like(rewards))
+                    if t > 1000:
+                        obses_t, actions, rewards, obses_tp1, dones, shares = replay_buffers.sample(32)
+                        train(obses_t, actions, rewards, obses_tp1, dones, np.transpose(shares), np.ones_like(rewards))
                     # Update target network periodically.
-                    if t % 200 == 0 and (t / 200) % 2 == 1:
-                        update_target_clones[clone]()
+                    if t % 1000 == 0:
+                        update_target()
 
                 # if done and len(episode_rewards[clone]) % 10 == 0:
                 #     logger.record_tabular("clone id", clone)
@@ -98,10 +94,8 @@ if __name__ == '__main__':
                 #     logger.record_tabular("% time spent exploring", int(100 * exploration.value(t)))
                 #     logger.dump_tabular()
 
-            if t % 200 == 0 and t != 0 and (t / 200) % 2 == 0:
+            if t % 1000 == 0 and t != 0:
                 last_returns = [np.mean(episode_reward[-5:-1]) for episode_reward in episode_rewards]
-                merged, group_best_return, clone_best_returns, group_best_q_func_vars = pso_update(last_returns)
-                summary_writer.add_summary(merged,t)
+                # summary_writer.add_summary(merged, t)
                 logger.log("Last Reward {}".format(['%.2f' % return_ for return_ in last_returns]))
                 # logger.log("Clone Best Rewards {}".format(['%.2f' % return_ for return_ in clone_best_returns]))
-                # logger.dump_tabular()
